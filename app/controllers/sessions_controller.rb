c9a9b9b51d518b4ae089cf01f48844b5979a137f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-12 11:39:58 -0500

    enabling user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
 
   def create
-
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
+    else
+      render json: user.errors.full_messages, status: :forbidden
+    end
   end
 
   def autologin
