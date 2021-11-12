d391f7d67517040a9f93b00f657ac6871cb7f797
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-11 22:46:49 -0500

    working on usda_api Location creation

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,9 +2,17 @@ class Location < ApplicationRecord
 
   has_many :events
 
-  def self.usda_search_record_exists?(record)
-    usda_id = record["id"].to_i
+  def self.usda_search_record_exists?(result)
+    usda_id = result["id"].to_i
     !!self.find_by_usda_id(usda_id)
+  end
+
+  def self.create_from_usda_api(id_and_name, details)
+    usda_id = id_and_name["id"].to_i
+    name = 
+    address = details["Address"].split(",")[0]
+    zipcode = details["Address"].split(",")[-1]
+    byebug
   end
 
 
--- app/services/usda_api.rb
+++ app/services/usda_api.rb
@@ -11,10 +11,25 @@ class UsdaApi
     request = Net::HTTP::Get.new(url)
     response = https.request(request)
     results = JSON.parse(response.body)["results"]
+
+    results.each do |result|
+      if !Location.usda_search_record_exists?(result)
+        details = self.usda_id_search(result["id"])
+        Location.create_from_usda_api(result, details)
+      end
+    end
+
   end
 
   def self.usda_id_search(usda_id)
     url = URI("https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=#{usda_id}")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    response = https.request(request)
+    result = JSON.parse(response.body)["marketdetails"]
   end
 
 
