658c9e66b8351d633a0697016ce8203db90a6a3f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-12-05 16:45:15 -0500

    adding products#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,14 @@ class ProductsController < ApplicationController
     byebug
   end
 
+  def show
+    product = Product.find(params[:id])
+    if !!product
+      render json: product.display
+    else
+      render json: {errors: "No Such Product"}, status :unprocessable_entity
+  end
+
   def update
     byebug
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ApplicationRecord
 
   belongs_to :vendor
   has_many :items
+
+  def display
+    {
+      name: self.name,
+      category: self.category,
+      description: self.description,
+      image: self.image,
+      price: self.price,
+      quantity: self.quantity,
+      vendor_id: self.vendor.id
+    }
+  end
 end
