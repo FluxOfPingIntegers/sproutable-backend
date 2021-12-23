982a195994e0ac04a955dd78fbd90017f6b70cbb
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-12-23 17:35:51 -0500

    creating error message for stalls#create

--- app/controllers/stalls_controller.rb
+++ app/controllers/stalls_controller.rb
@@ -5,6 +5,9 @@ class StallsController < ApplicationController
       user = logged_in_user
       vendor = user.vendor
       event = params[:id]
+      byebug
+    else
+      render json: {errors: "Invalid action."}, status: :unprocessable_entity
     end
   end
 
