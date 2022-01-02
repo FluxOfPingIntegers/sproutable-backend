f1aa1dde24de7e6c3f2d60dc578f0c98dd62cf70
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-02 12:19:01 -0500

    cleaning up stalls#create

--- app/controllers/stalls_controller.rb
+++ app/controllers/stalls_controller.rb
@@ -3,18 +3,12 @@ class StallsController < ApplicationController
   def create
     if !!logged_in_user.vendor
       vendor = logged_in_user.vendor
-      event_id_list = stall_form_params.map {|param| param[:event_id]}
+      event_id_list = params[:stalls].map {|param| param[:event_id]}
       Stall.process_form({event_id_list: event_id_list, vendor_id: vendor.id})
       render json: vendor.display
     else
       render json: {errors: "Invalid stall creation attempt"}, status: :unprocessable_entity
     end
-  end
-
-  private
-
-  def stall_form_params
-    params.require(:stalls)
   end
 
 end
